4636577a9c4aceae9c4c019ec454667e7758bb3f
acoosto <stojaal1@students.zhaw.ch>
2023-04-15 14:57:04 +0200

    update

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/TDDTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/TDDTest.java
@@ -6,12 +6,28 @@ import org.junit.jupiter.api.Test;
 
 public class TDDTest {
 
+class ConvertTest1 {
+
     @Test
     public void numberConverter1() {
-        TDD numberConverter = new TDD();
-        
+        NumberConverter numberConverter = new NumberConverter();
+            
         assertThat("X").isEqualTo(numberConverter.convert(10));
-                
+                    
+    }
+
+}
+
+    public String roman;
+
+    @Test
+    public void numberConverter2() {
+        NumberConverter numberConverter = new NumberConverter();
+        
+        roman = numberConverter.getCharacter();
+
+        assertThat(roman).isEqualTo(numberConverter.convert(10));
+
     }
 
 }
