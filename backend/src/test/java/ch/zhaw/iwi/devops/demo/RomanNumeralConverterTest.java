80b10582f7df44b7992c5de2ca5605ed1b1ede5e
acoosto <stojaal1@students.zhaw.ch>
2023-04-16 23:44:01 +0200

    Update

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverterTest.java
@@ -1,11 +1,20 @@
 package ch.zhaw.iwi.devops.demo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class RomanNumeralConverterTest {
-    
     @Test
-    public void testConvert_1_ReturnsI() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
-        String result = converter.convert(1);
-        Assertions.assertEquals("I", result);
+    public void testConvert() {
+        Assertions.assertEquals("I", RomanNumeralConverter.convert(1));
+        Assertions.assertEquals("II", RomanNumeralConverter.convert(2));
+        Assertions.assertEquals("III", RomanNumeralConverter.convert(3));
+        Assertions.assertEquals("IV", RomanNumeralConverter.convert(4));
+        Assertions.assertEquals("V", RomanNumeralConverter.convert(5));
+        Assertions.assertEquals("IX", RomanNumeralConverter.convert(9));
+        Assertions.assertEquals("XXI", RomanNumeralConverter.convert(21));
+        Assertions.assertEquals("L", RomanNumeralConverter.convert(50));
+        Assertions.assertEquals("C", RomanNumeralConverter.convert(100));
+        Assertions.assertEquals("D", RomanNumeralConverter.convert(500));
+        Assertions.assertEquals("M", RomanNumeralConverter.convert(1000));
     }
 }
