2a8040a198e562963d2d80d678cbf53e5bd9d57b
acoosto <stojaal1@students.zhaw.ch>
2023-04-17 10:20:50 +0200

    Klassen überarbeitet - alle Tests erfolgreich

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverter.java
@@ -1,23 +1,31 @@
 package ch.zhaw.iwi.devops.demo;
 
+import java.util.TreeMap;
+
 public class RomanNumeralConverter {
-    
-    public String convert(int number) {
-        if (number == 1) {
-            return "I";
+    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+
+    static {
+        map.put(1, "I");
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
+    }
+
+    public static String convert(int number) {
+        int l = map.floorKey(number);
+        if (number == l) {
+            return map.get(number);
         }
-        if (number == 2) {
-            return "II";
-        }
-        if (number == 3) {
-            return "III";
-        }
-        if (number == 4) {
-            return "IV";
-        }
-        if (number == 5) {
-            return "V";
-        }
-        return "";
+        return map.get(l) + convert(number - l);
     }
 }
