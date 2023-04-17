50086b9da1ddb704189bd394f87ed4db316859d6
acoosto <stojaal1@students.zhaw.ch>
2023-04-17 10:16:45 +0200

    Test 6, 7, 8, 9 und 10 - Test fehlgeschlagen

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverterTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/RomanNumeralConverterTest.java
@@ -38,4 +38,47 @@ public class RomanNumeralConverterTest {
         String result = converter.convert(5);
         Assertions.assertEquals("V", result);
     }
+
+    @Test
+    public void testConvert_9() {
+        RomanNumeralConverter converter = new RomanNumeralConverter();
+        String result = converter.convert(9);
+        Assertions.assertEquals("IX", result);
+    }
+    
+    @Test
+    public void testConvert_21() {
+        RomanNumeralConverter converter = new RomanNumeralConverter();
+        String result = converter.convert(21);
+        Assertions.assertEquals("XXI", result);
+    }
+
+    @Test
+    public void testConvert_50() {
+        RomanNumeralConverter converter = new RomanNumeralConverter();
+        String result = converter.convert(50);
+        Assertions.assertEquals("L", result);
+    }
+    
+    @Test
+    public void testConvert_100() {
+        RomanNumeralConverter converter = new RomanNumeralConverter();
+        String result = converter.convert(100);
+        Assertions.assertEquals("C", result);
+    }
+
+    @Test
+    public void testConvert_final() {
+        Assertions.assertEquals("I", RomanNumeralConverter.convert(1));
+        Assertions.assertEquals("II", RomanNumeralConverter.convert(2));
+        Assertions.assertEquals("III", RomanNumeralConverter.convert(3));
+        Assertions.assertEquals("IV", RomanNumeralConverter.convert(4));
+        Assertions.assertEquals("V", RomanNumeralConverter.convert(5));
+        Assertions.assertEquals("IX", RomanNumeralConverter.convert(9));
+        Assertions.assertEquals("XXI", RomanNumeralConverter.convert(21));
+        Assertions.assertEquals("L", RomanNumeralConverter.convert(50));
+        Assertions.assertEquals("C", RomanNumeralConverter.convert(100));
+        Assertions.assertEquals("D", RomanNumeralConverter.convert(500));
+        Assertions.assertEquals("M", RomanNumeralConverter.convert(1000));
+    }
 }
